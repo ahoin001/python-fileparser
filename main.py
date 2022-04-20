789babdea3687f3cc3173e804315d34bd7ccfe58
Alexander Hoinville <ahoin001@gmail.com>
2022-04-19 21:25:16 -0400

    changed to use argparser for better command line interface interaction

--- main.py
+++ main.py
@@ -2,33 +2,45 @@ from collections import Counter
 
 # Used to take arguments from command line
 import sys
+import argparse
 
-script_being_run = sys.argv[0]
+# Create the argparser (Easily constructs command line interface)
+my_parser = argparse.ArgumentParser(
+    description="Input a file and and recieve parsed data from it"
+)
 
-try:
+# Add the arguments or flags for user to input
 
-    user_file = sys.argv[1]
+my_parser.add_argument(
+    "users_file", metavar="users_file", type=str, help="the file you want parsed"
+)
 
-    with open(user_file, "rt") as f:
-        wordFile = f.read()
-        formattedFileOutput = f"Total Words: {len(wordFile.split())} \n \n"
-        # Count the occurences of each word into dictionary
-        dictOfWordOccur = Counter(wordFile.split())
+# Execute the parse_args() method. Args now gives access to arguments we created before
+args = my_parser.parse_args()
 
-        # Print line for each word and their occurences
-        for word in dictOfWordOccur:
-            formattedFileOutput = (
-                formattedFileOutput + f"{word}: {dictOfWordOccur[word]} \n"
-            )
+user_file = args.users_file
 
-        try:
-            # Write the completed output to a new file .words, overwrite if it exsists
-            with open(".words", "w") as file:
-                file.write(formattedFileOutput)
-        except IOError:
-            print(f"Problem writing to output file")
+# print(user_file)
 
+with open(user_file, "rt") as f:
+    wordFile = f.read()
+    formattedFileOutput = f"Total Words: {len(wordFile.split())} \n \n"
+
+    # Count the occurences of each word into dictionary
+    dictOfWordOccur = Counter(wordFile.split())
+
+    # Print line for each word and their occurences
+    for word in dictOfWordOccur:
+        formattedFileOutput = (
+            formattedFileOutput + f"{word}: {dictOfWordOccur[word]} \n"
+        )
+
+    try:
+        # Write the completed output to a new file .words, overwrite if it exsists
+        with open(".words", "w") as file:
+            file.write(formattedFileOutput)
+
+    except IOError:
+        print(f"Problem writing to output file")
         print(formattedFileOutput)
 
-except IOError:
-    print(f"Problem opening file")
