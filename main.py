6291eb0e7c45f71274feb10f5734b342bc411207
Alexander Hoinville <ahoin001@gmail.com>
2022-04-19 21:39:07 -0400

    Accept user argument for output file name

--- main.py
+++ main.py
@@ -15,10 +15,17 @@ my_parser.add_argument(
     "users_file", metavar="users_file", type=str, help="the file you want parsed"
 )
 
+my_parser.add_argument(
+    "-o",
+    dest="desired_file_output_name",
+    help="Provide name you want for the output file instead of .words",
+)
+
 # Execute the parse_args() method. Args now gives access to arguments we created before
 args = my_parser.parse_args()
 
 user_file = args.users_file
+
 
 # print(user_file)
 
@@ -36,9 +43,19 @@ with open(user_file, "rt") as f:
         )
 
     try:
-        # Write the completed output to a new file .words, overwrite if it exsists
-        with open(".words", "w") as file:
-            file.write(formattedFileOutput)
+
+        # If user pprovided a file name with -o arg, use that name for new file being written
+        if args.desired_file_output_name:
+
+            print(f"Creating new output file named: {args.desired_file_output_name}")
+
+            # Write the completed output to a new file .words, overwrite if it exsists
+            with open(f"{args.desired_file_output_name}", "w") as file:
+                file.write(formattedFileOutput)
+        else:
+            # If no name given by user, create file as .words
+            with open(".words", "w") as file:
+                file.write(formattedFileOutput)
 
     except IOError:
         print(f"Problem writing to output file")
